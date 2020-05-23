fc34210aa50a259aecadd1ab5e0abad27da67f7d
Ubaid <ubaidmohd.786@gmail.com>
2020-05-23 11:15:57 +0530

    added loader

--- src/Search.js
+++ src/Search.js
@@ -1,51 +1,75 @@
-import React,{useEffect, useState} from 'react';
-import Recipe from './Recipe';
-import './Search.css';
-
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import Spinner from "./Spinner";
+import "./Search.css";
 
 function Search() {
-    const APP_ID = "a74475bc";
-    const APP_KEY = "7a94da0cecc0dffe3d93ad7ea951df20";
-    
-    const [recipes, setRecipes] = useState([]);
-    const [search, setSearch] = useState('');
-    const [query, setQuery] = useState('chicken');
+  const APP_ID = "a74475bc";
+  const APP_KEY = "7a94da0cecc0dffe3d93ad7ea951df20";
 
-    const getRecipe = async ()=>{
-        const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-        const data = await response.json();
-        setRecipes(data.hits);
-        console.log(data.hits)
-    }
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+  const [loading, setLoading] = useState(true);
+  const getRecipe = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    setLoading(false);
+  };
 
-    const setText = (e)=>{
-        setSearch(e.target.value);
-    }
+  const setText = (e) => {
+    setSearch(e.target.value);
+  };
 
-    const getSearch = (e)=>{
-        e.preventDefault();
-        setQuery(search);
-    }
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  };
 
-    useEffect( ()=>{
-        getRecipe();
-    },[query])
+  useEffect(() => {
+    getRecipe();
+  }, [query]);
 
-    return <div>
-      <form  className="Search" onSubmit={getSearch}>
-            <input type="text" value={search} onChange={setText} placeholder="Search for food recipes"></input>
-            <button type="submit"><i  className="fas fa-utensils"></i></button>
-      </form>
+  let main;
+  if (loading) {
+    main = <Spinner />;
+  } else {
+    main = (
       <div className="food-row">
-            {recipes.map(recipe =>(
-                <Recipe key={recipe.recipe.label} title={recipe.recipe.label} img={recipe.recipe.image} 
-                        calories={recipe.recipe.calories} ingredients={recipe.recipe.ingredients} url={recipe.recipe.url}
-                        source={recipe.recipe.source} />
-            ))}
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            img={recipe.recipe.image}
+            calories={recipe.recipe.calories}
+            ingredients={recipe.recipe.ingredients}
+            url={recipe.recipe.url}
+            source={recipe.recipe.source}
+          />
+        ))}
       </div>
-      
-       
-    </div>;
+    );
   }
-  
-  export default Search;
+
+  return (
+    <div>
+      <form className="Search" onSubmit={getSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={setText}
+          placeholder="Search for food recipes"
+        ></input>
+        <button type="submit">
+          <i className="fas fa-utensils"></i>
+        </button>
+      </form>
+      {main}
+    </div>
+  );
+}
+
+export default Search;
---
+++ src/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import loader from "./loader/45.gif";
+
+function Spinner() {
+  return (
+    <div>
+      <img src={loader} alt="loader"></img>
+    </div>
+  );
+}
+
+export default Spinner;
